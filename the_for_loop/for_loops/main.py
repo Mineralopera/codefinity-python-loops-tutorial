901514c3eb490dabfe6ff196ab305f12eb0dec34
Mineralopera <>
2025-11-27 06:56:33 +0000

    For loops

---
+++ the_for_loop/for_loops/main.py
@@ -0,0 +1,10 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'Barcelona', 'South Africa', 'Indonesia','Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Tokyo', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Paris', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Liverpool', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'New York', 'China', 'Munchen', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List of visited countires
+travel_list = []
+
+# Creating loop
+for country in countries:
+    travel_list.append(country)
+    print("Countries to visit:", travel_list)
